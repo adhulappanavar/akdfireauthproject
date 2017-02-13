8472617fbc41636b527332c2e2ec685fc0698182
Anil Dhulappanavar <anil.dhulappanavar@gmail.com>
2017-02-13 21:03:41 +0530

    Working baseline - !!!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,40 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+import { AuthGuard } from './auth.service';
+
+import { routes } from './app.routes';
+
+// Must export the config
+export const firebaseConfig = {
+    apiKey: "AIzaSyAY8keH2Dab2dU66yMFQCLOCeyWiPd8mSY",
+    authDomain: "anilauthproject.firebaseapp.com",
+    databaseURL: "https://anilauthproject.firebaseio.com",
+    storageBucket: "anilauthproject.appspot.com",
+    messagingSenderId: "607517651410"
+};
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    routes
   ],
-  providers: [],
+  providers: [AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
